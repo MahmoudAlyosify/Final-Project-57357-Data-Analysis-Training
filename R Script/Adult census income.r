6b6c59ae1e0a0b5f255ba817c9423963d7f02a3c
Mahmoud Sayed Youssef Kotb <59246305+MahmoudAlyosify@users.noreply.github.com>
2021-09-28 18:35:34 +0200

    The Lastest 1

--- R Script/Adult census income.r
+++ R Script/Adult census income.r
@@ -33,7 +33,7 @@ rm(Data2,Data_Orderby_Age_income,i,j,Total_Capital)
 the_50th_Rate_of_income<-sum(as.integer(Data_Orderby_Age_income_C$income_c[1:50]))/50
 the_50th_Rate_of_Age<-sum(as.integer(Data_Orderby_Age_income_C$age[1:50]))/50
 print("There is no relationship between the age and the income of the individual, as the average age of the top fifty individuals is 39 years and the average income is 9244.7 per year")
-
+rm(the_50th_Rate_of_income,the_50th_Rate_of_Age,rang)
 #********************************************************************************************
 
 #2. Can we say that we have achieved gender equality?
@@ -68,10 +68,27 @@ print(paste("The Number of Male is",nrow(Data_Male),
             The_AVG_of_HSgrad_Female,"Also, the percentage of males with a bachelor's degree is",
             The_AVG_of_Bachelor_Male,"But the percentage of women with a bachelor's degree is",
             The_AVG_of_Bachelor_Female))
-
+rm(Data_Male,Data_Female,Sum_income_Female,Sum_income_Male,
+   the_Rato_of_income_for_Male,the_Rato_of_income_for_Female,
+   Education_Levels_Male,Education_Levels_Female,The_AVG_of_HSgrad_Male,
+   The_AVG_of_Bachelor_Male,The_AVG_of_HSgrad_Female,
+   The_AVG_of_Bachelor_Female
+   )
 #********************************************************************************************
 
 #3. What other attributes would be an income predictor/s?
-print("Yes, there are some other characteristics that may affect the income, such as the degree of education, the job, and the number of hours worked for each week")
+Data_orderBy_Hours<-Data_Orderby_Age_income_C[order(Data_Orderby_Age_income_C$hours.per.week,decreasing = TRUE),]
+Data_orderBy_EducationNUM<-Data_Orderby_Age_income_C[order(Data_Orderby_Age_income_C$education.num ,decreasing = TRUE),]
+the_50th_Rate_of_Hours<-sum(as.integer(Data_orderBy_Hours$hours.per.week [1:50]))/50
+the_50th_Rate_of_Hours_INcome<-sum(as.integer(Data_orderBy_Hours$income_c [1:50]),na.rm = TRUE)/50
+the_50th_Rate_of_Education_NUM<-sum(as.integer(Data_orderBy_EducationNUM$education.num [1:50]))/50
+the_50th_Rate_of_EducationNUM_INcome<-sum(as.integer(Data_orderBy_EducationNUM$income_c [1:50]),na.rm = TRUE)/50
+print(paste("Yes, there are some other characteristics that may affect the income, such as the degree of education(Education NUM), and the number of hours worked for each week",
+      "The average working hours for the first 50 people are:",the_50th_Rate_of_Hours,"hours",
+      "And the average income according to the working hours for the First 50 people is",the_50th_Rate_of_Hours_INcome,
+      "$","Whereas, the average education level or education level for the First 50 people is",the_50th_Rate_of_Education_NUM,
+      "The average income according to the degree or level of education for the First 50 people",
+      the_50th_Rate_of_EducationNUM_INcome,"$"
+      ))
 
 #©Mahmoud Sayed Youssef
